b44e49cfba44dd60c24e8d9198bb7a677822a507
chauminh24 <114486211+chauminh24@users.noreply.github.com>
2025-05-04 21:05:56 +0000

    feat: update Angular core version to 19.2.9 and add Firebase configuration to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   weatherApiKey: 'f7246f77874f04ad337197d8909513c1', // Add this line
+  firebase: {
+    apiKey: "AIzaSyB9rMCbj4N_hhBTHs3-EL-920BmJJMiIUk",
+    authDomain: "quackquackforecast.firebaseapp.com",
+    projectId: "quackquackforecast",
+    storageBucket: "quackquackforecast.firebasestorage.app",
+    messagingSenderId: "970477672013",
+    appId: "1:970477672013:web:ceaf5751c2ca36afbbee04",
+    measurementId: "G-T6TNR7TTL1"
+  }
 };
